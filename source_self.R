fd8677d25cd3c9b748a0e4a6fd3867c09b2a1eec
edwardcooper <maxminicherrycc@gmail.com>
2017-09-09 13:47:45 -0400

    add new path to all the functions.

--- source_self.R
+++ source_self.R
@@ -1,23 +1,16 @@
 ## This script serves as a source to load various utility functions 
 
-source('~/Dropbox/Utility/char_to_factor.R', echo=TRUE)
-
-source('~/Dropbox/Utility/correlation.R', echo=TRUE)
-
-source('~/Dropbox/Utility/miss_name.R', echo=TRUE)
-
-source('~/Dropbox/Utility/miss_pct_plot.R', echo=TRUE)
-
-source('~/Dropbox/Utility/na_to_fac.R', echo=TRUE)
-
-source('~/Dropbox/Utility/select_comp.R', echo=TRUE)
-
-source('~/Dropbox/Utility/select_fac.R', echo=TRUE)
-
-source('~/Dropbox/Utility/select_num.R', echo=TRUE)
-
-source('~/Dropbox/Utility/timeRecord_functions.R', echo=TRUE)
-
-source('~/Dropbox/Utility/miss_pct_df.R', echo=TRUE)
-
-source('~/Dropbox/Utility/int_to_num.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/timeRecord_functions.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/caret_to_mlList.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/char_to_factor.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/correlation.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/fac_to_num.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/int_to_num.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/miss_name.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/miss_pct_df.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/miss_pct_plot.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/mlog.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/na_to_fac.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/select_comp.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/select_fac.R', echo=TRUE)
+source('~/Dropbox/mlmodel_select/select_num.R', echo=TRUE)
