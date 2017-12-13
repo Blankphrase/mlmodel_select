63cf591b09f6695571cf73ed8faced404d8ad6f4
edwardcooper <maxminicherrycc@gmail.com>
2017-12-12 23:25:55 -0500

    add min and max choice in ml_metric_filter.

--- ml_list.R
+++ ml_list.R
@@ -44,8 +44,6 @@ ml_tune=function(data,target,sampling=NULL,metric="Accuracy",search = "random",k
          }
   )
   
-  # random tuning grid     
-  # xgbTree_tune_grid=
   
   # train the function 
   # consider change the input into x and y in the future.
@@ -213,7 +211,85 @@ ml_bwplot=function(models){
 # example use of ml_bwplot
 # ml_bwplot(testmodels_churn)
 
+ml_metric_filter=function(models,metric="ROC",mini=NULL,max=NULL,FUN=median){
+  # get all the metrics in the model list
+  model_metrics=models%>%lapply( function(model_list){model_list$metric})%>%unlist
+  # if the metric is in the model_metrics, then select the models based on minimum value of the metric
+  if(!metric %in% model_metrics){paste("There is no metric, ",metric,", in the model performance. "
+                                       ,"Try using metrics:",glue::collapse(unique(model_metrics),sep=", ") )%>%stop() }
+  
+  
+  # ==============================================================================================================================
+  # define a function to filter models. 
+  filter_model=function(model,metric,FUN,mini=NULL,max=NULL){
+    # model%>%resamples%>%.$values%>%print
+    if(!is.null(FUN)){
+      # if the FUN variable is not null, then we will use that function to apply to the performance metrics dataframe. 
+      # resample the models, select the performance metrics dataframe from the object,
+      # then select the columns that contains the metric you selected in the function options.
+      # then apply the function FUN to the list, and unlist the values and turn the vector into a TRUE or FALSE vector to select models in filtered_models.
+      
+      # value_for_switch is 1 when only mini variable is not null
+      # value_for_switch is 2 when both mini and max variable are not null.
+      # value_for_switch is 3 when only max variable is not null. 
+      value_for_switch=as.numeric(is.null(mini))+as.numeric(!is.null(max))+1
+      switch (value_for_switch
+              ,{
+                # for using only mini
+                print("using mini")
+                filter_values=(model%>%resamples%>%.$values%>%select(contains(metric))%>%apply(2,FUN)%>%unlist)>mini
+              }
+              ,{
+                # for using max and mini
+                print("using max and mini")
+                filter_values_mini=(model%>%resamples%>%.$values%>%select(contains(metric))%>%apply(2,FUN)%>%unlist)>mini
+                filter_values_max=(model%>%resamples%>%.$values%>%select(contains(metric))%>%apply(2,FUN)%>%unlist)<max
+                filter_values=(filter_values_max & filter_values_mini)
+              }
+              ,{
+                # for using only max
+                print("using max")
+                filter_values=(model%>%resamples%>%.$values%>%select(contains(metric))%>%apply(2,FUN)%>%unlist)<max
+              }
+      )
+      model%>%resamples%>%.$values%>%select(contains(metric))%>%apply(2,FUN)%>%unlist%>%print
+      filter_values%>%print
+      filtered_model_from_metric=model[filter_values]
+    }else{
+      message("please give FUN variable a value like min or median")
+    }
+    
+    return(filtered_model_from_metric)
+  }
+  # ==============================================================================================================================
+  
+  # select models based on which metric group the metric is in.  
+  # if ROC is in that list, then it will return the second option.
+  # if ROC is not in that list, then it will return the first option. a.k.a kappa and accuracy models
+  switch(as.numeric(metric %in% c("ROC","Sens","Spec"))+1
+         ,{
+           filtered_models=models[model_metrics %in% c("Accuracy","Kappa")]%>%filter_model(metric=metric,FUN=FUN,mini=mini,max=max)
+         }
+         ,{
+           filtered_models=models[model_metrics %in% c("ROC","Sens","Spec")]%>%filter_model(metric=metric,FUN=FUN,mini=mini,max=max)
+         }
+  )
+  
+  return(filtered_models)
+}
 
+
+
+# the supported metrics are ROC,Sens,Spec,Accuracy,Kappa. 
+# example use to select models based on metric. 
+# this select models that has a minimum of 0.75 median accuracy 
+# testmodels_metric_filtered=testmodels_churn%>%ml_metric_filter(metric="Accuracy",mini=0.75,FUN=median)
+# this select models that has a minimum accuracy of 0.75
+# testmodels_metric_filtered=testmodels_churn%>%ml_metric_filter(metric="Accuracy",mini=0.75,FUN=min)
+# this select models that has a minimum standard deviation of 0.01
+# testmodels_metric_filtered=testmodels_churn%>%ml_metric_filter(metric="Accuracy",mini=0.75,FUN=sd)
+# select models that has a minimum ROC median of 0.84 and a maximum ROC standard deviation of 0.017
+# testmodels_metric_filtered=testmodels_churn%>%ml_metric_filter(metric="ROC",mini=0.84,FUN=median)%>%ml_metric_filter(metric="ROC",max=0.017,FUN=sd)
 
 #########################################################################################################################
 
